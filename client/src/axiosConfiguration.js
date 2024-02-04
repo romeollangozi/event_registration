74dc414df900c628bc73b9630fa3ead283d402bc
romeollangozi <romeollangozi123@gmail.com>
2024-02-04 15:49:36 +0100

    axios config

--- client/src/axiosConfiguration.js
+++ client/src/axiosConfiguration.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 
 const URL = import.meta.env.VITE_API_URL || 'localhost:8080'
 axios.defaults.baseURL = `https://${URL}/api/`;
-// axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 
 export default axios;
